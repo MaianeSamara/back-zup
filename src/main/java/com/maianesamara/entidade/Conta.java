3c21be37efcd61b5c8bdace62c8e2b4df0dfe74e
MaianeSamara <maianesamara@gmail.com>
2021-01-05 18:35:16 -0300

    Lombok

--- src/main/java/com/maianesamara/entidade/Conta.java
+++ src/main/java/com/maianesamara/entidade/Conta.java
@@ -2,62 +2,23 @@ package com.maianesamara.entidade;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Conta {
 
+	@EqualsAndHashCode.Include
 	private Long id;
 	private String nome;
 	private String email;
 	private String cpf;
 	private Date dataNascimento;
-
-	public Conta() {
-	}
-
-	public Conta(Long id, String nome, String email, String cpf, Date dataNascimento) {
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.cpf = cpf;
-		this.dataNascimento = dataNascimento;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
 }
