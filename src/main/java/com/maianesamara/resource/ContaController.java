6d197b32ec2b1498e05f881f84038c7f7795901e
MaianeSamara <maianesamara@gmail.com>
2021-01-07 19:23:00 -0300

    Editando conta com PUT

--- src/main/java/com/maianesamara/resource/ContaController.java
+++ src/main/java/com/maianesamara/resource/ContaController.java
@@ -3,12 +3,14 @@ package com.maianesamara.resource;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +55,14 @@ public class ContaController {
 		contaDto = service.inserirConta(contaDto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(contaDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(contaDto);
+	}
+	
+	@PutMapping(value = "/{idConta}")
+	public ResponseEntity<ContaDto> atualizarConta(@PathVariable Long idConta, @RequestBody ContaDto contaDto){
+		ContaDto contaDtoAtualização = contaMap.toModel(service.buscarContaId(idConta));
+		BeanUtils.copyProperties(contaDto, contaDtoAtualização , "id", "dataNascimento");
+		contaDto = service.inserirConta(contaDtoAtualização);
+		return ResponseEntity.ok().body(contaDto);	
 	}
 }
 
